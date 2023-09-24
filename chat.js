937c4d20452c4376f3af95477255fd32807082a8
sanju0911 <97456105+sanju0911@users.noreply.github.com>
2023-09-24 21:00:50 +0530

    completed with local storage

--- chat.js
+++ chat.js
@@ -1,53 +1,25 @@
-const express = require('express');
-const fs = require('fs');
-const bodyParser = require('body-parser');
+const express = require("express");
+const fs = require("fs");
+
+const path = require("path");
+const bodyParser = require("body-parser");
 
 const app = express();
 
+const Login = require("./routes/login");
+
+const Msg = require("./routes/msg");
+
 app.use(bodyParser.urlencoded({ extended: true }));
 
+app.use(Login);
 
-app.get('/login', (req, res) => {
-    res.send('<form action="/login" method="POST"><input type="text" name="title"><button type="submit">login</button></form>');
-});
+app.use(Msg);
 
-app.post('/login', (req, res) => {
-  
-    const username = req.body.title;
-
-   
-    res.redirect(`/chat?username=${encodeURIComponent(username)}`);
-});
-
-
-app.get('/chat', (req, res) => {
-    const username = req.query.username;
-
-   
-    res.send(`
-        <form action="/" method="POST">
-            <h2>Welcome ${username}!</h2>
-            <input type="hidden" id="username" name="username" value="${username}">
-            <input type="text" id="message" name="message" placeholder="Enter your message">
-            <button type="submit">Send</button>
-        </form>
-        <h2>Messages:</h2>
-        <div id="messages"></div>
-        <script src="/client.js"></script>
-    `);
-});
-
-app.post('/', (req, res) => {
-    const username = req.body.username;
-    const message = req.body.message;
-
-   
-    fs.appendFileSync('messages.txt', `${username}: ${message}\n`);
-
-    
-    res.redirect(`/chat?username=${encodeURIComponent(username)}`);
+app.use((req, res, next) => {
+  res.status(404).sendFile(path.join(__dirname, "views", "pagenotfound.html"));
 });
 
 app.listen(8005, () => {
-    console.log('Server is listening on port 8005');
+  console.log("Server is listening on port 8005");
 });
